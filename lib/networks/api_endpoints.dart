9257e102515ece4184b17bb0f40416d5444a7895
Tia Wicaksono <wicaksono.tia@gmail.com>
2025-05-19 20:41:36 +0700

    change endpoint url

--- lib/networks/api_endpoints.dart
+++ lib/networks/api_endpoints.dart
@@ -1,16 +1,16 @@
 class ApiEndPoints {
-  static const String baseUrl = 'http://103.184.181.9/api/';
+  static const String baseUrl = 'http://103.184.181.9/apiEsJeruk/';
   static _AuthEndPoints authEndpoints = _AuthEndPoints();
 }
 
 class _AuthEndPoints {
   final String login = 'loginkios';
+  final String categories = 'productCategory';
   final String product = 'products';
   final String saveDetailTransaction = 'savedetailtransaction';
   final String saveTransaction = 'savetransaction';
   final String getRowTransactions = 'gettransaction';
   final String getHistoryTransactions = 'transactions';
   final String deleteTransaction = 'deletetransaction';
-  final String getTransactionDetails = 'transactiondetails';
-  final String categories = 'productCategory';
+  final String getTransactionDetails = 'transactiondetail';
 }
